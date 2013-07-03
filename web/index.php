def4d88d745da6e0d509f45aeb488cc68c1903a7
Kalle Lamminpää <lamminpaakm@gmail.com>
2013-07-03 15:57:56 +0300

    added front

--- web/index.php
+++ web/index.php
@@ -2,9 +2,13 @@
 require_once __DIR__.'/../vendor/autoload.php';
 
 $app = new Silex\Application();
+$app->register(new Silex\Provider\TwigServiceProvider(), array(
+    'twig.path' => __DIR__.'/../views',
+));
 
-$app->get('/hello/{name}', function ($name) use ($app) {
-return 'Hello '.$app->escape($name);
+$app->get('/', function () use ($app) {
+    return $app['twig']->render('front.twig', array(
+    ));
 });
 
 $app->run();
